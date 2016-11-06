383727a79f569d2ac1ece58a02c172193f13e9f9
BaronZ <realbo89@outlook.com>
2016-11-06 17:06:17 +0800

    annotations with comment

---
+++ easysp-processor/src/main/java/com/zzb/easysp/processor/annotations/CustomKey.java
@@ -0,0 +1,14 @@
+package com.zzb.easysp.processor.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by ZZB on 2016/11/6.
+ */
+@Target(ElementType.TYPE)//tells us what this annotation can be applied to. The type element is anything like classes, interfaces, etc
+@Retention(RetentionPolicy.SOURCE)//@Retention annotation that tells the compiler how long it needs to keep these annotations around. In our case, we’re using a source RetentionPolicy
+public @interface CustomKey {
+}
