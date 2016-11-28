a5dce853bc599b736501a15fe77ae64504bfd1ba
BaronZ <realbo89@outlook.com>
2016-11-28 18:15:29 +0800

    init EasySpProcessor

--- easysp-compiler/src/main/java/com/zzb/easysp/compiler/EasySpProcessor.java
+++ easysp-compiler/src/main/java/com/zzb/easysp/compiler/EasySpProcessor.java
@@ -1,8 +1,46 @@
 package com.zzb.easysp.compiler;
 
+import com.zzb.easysp.EasySP;
+import java.util.HashSet;
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+
 /**
  * Created by ZZB on 2016/11/28.
  */
 
-public class EasySpProcessor {
+public class EasySpProcessor extends AbstractProcessor {
+
+    private Messager mMessager;//The messager is how we send messages back to the user. We can't use System.out
+    private Filer mFiler;// You use the filer to write your new files.
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        mMessager = processingEnv.getMessager();
+        mFiler = processingEnv.getFiler();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> set = new HashSet<>();
+        set.add(EasySP.class.getCanonicalName());
+        return set;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
 }
