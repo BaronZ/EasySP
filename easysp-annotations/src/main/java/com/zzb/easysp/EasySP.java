7d4ca099bf118394535df30a9187fcdf02c2509a
BaronZ <realbo89@outlook.com>
2016-11-28 17:43:15 +0800

    add easysp-annotations module

---
+++ easysp-annotations/src/main/java/com/zzb/easysp/EasySP.java
@@ -0,0 +1,14 @@
+package com.zzb.easysp;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by ZZB on 2016/11/6.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface EasySP {
+}
