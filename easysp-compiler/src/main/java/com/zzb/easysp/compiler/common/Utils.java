0ab1f96ce2ec6694b0d4176bdade1df978e17cb5
BaronZ <realbo89@outlook.com>
2016-11-30 19:48:51 +0800

    format

--- easysp-compiler/src/main/java/com/zzb/easysp/compiler/common/Utils.java
+++ easysp-compiler/src/main/java/com/zzb/easysp/compiler/common/Utils.java
@@ -5,14 +5,12 @@ import java.util.Locale;
 import javax.lang.model.element.Element;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
-import javax.lang.model.util.Elements;
 
 /**
  * Created by ZZB on 2016/11/29.
  */
 
 public class Utils {
-
 
     public static String getSetterMethodName(String fieldName) {
         return "set" + upperCaseFirst(fieldName);
@@ -25,42 +23,44 @@ public class Utils {
     public static boolean isSupportedFieldType(TypeMirror fieldType) {
         TypeKind typeKind = fieldType.getKind();
         String typeClassName = fieldType.toString();
-        if(typeKind == TypeKind.BOOLEAN || Boolean.class.getName().equals(typeClassName)){
+        if (typeKind == TypeKind.BOOLEAN || Boolean.class.getName().equals(typeClassName)) {
             return true;
-        }else if(typeKind == TypeKind.INT || Integer.class.getName().equals(typeClassName)){
+        } else if (typeKind == TypeKind.INT || Integer.class.getName().equals(typeClassName)) {
             return true;
-        }else if(typeKind == TypeKind.LONG || Long.class.getName().equals(typeClassName)){
+        } else if (typeKind == TypeKind.LONG || Long.class.getName().equals(typeClassName)) {
             return true;
-        }else if(typeKind == TypeKind.FLOAT || Float.class.getName().equals(typeClassName)){
+        } else if (typeKind == TypeKind.FLOAT || Float.class.getName().equals(typeClassName)) {
             return true;
-        }else if(String.class.getName().equals(typeClassName)){
+        } else if (String.class.getName().equals(typeClassName)) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
 
-    public static String getPackageName(Element element){
+    public static String getPackageName(Element element) {
         String fullName = element.asType().toString();
         return fullName.substring(0, fullName.lastIndexOf("."));
     }
-    public static String getClassName(Element element){
+
+    public static String getClassName(Element element) {
         return element.getSimpleName().toString();
     }
+
     public static Type getType(TypeMirror fieldType) {
         TypeKind typeKind = fieldType.getKind();
         String typeClassName = fieldType.toString();
-        if(typeKind == TypeKind.BOOLEAN || Boolean.class.getName().equals(typeClassName)){
+        if (typeKind == TypeKind.BOOLEAN || Boolean.class.getName().equals(typeClassName)) {
             return typeKind == TypeKind.BOOLEAN ? boolean.class : Boolean.class;
-        }else if(typeKind == TypeKind.INT || Integer.class.getName().equals(typeClassName)){
+        } else if (typeKind == TypeKind.INT || Integer.class.getName().equals(typeClassName)) {
             return typeKind == TypeKind.INT ? int.class : Integer.class;
-        }else if(typeKind == TypeKind.LONG || Long.class.getName().equals(typeClassName)){
+        } else if (typeKind == TypeKind.LONG || Long.class.getName().equals(typeClassName)) {
             return typeKind == TypeKind.LONG ? long.class : Long.class;
-        }else if(typeKind == TypeKind.FLOAT || Float.class.getName().equals(typeClassName)){
+        } else if (typeKind == TypeKind.FLOAT || Float.class.getName().equals(typeClassName)) {
             return typeKind == TypeKind.FLOAT ? float.class : Float.class;
-        }else if(String.class.getName().equals(typeClassName)){
+        } else if (String.class.getName().equals(typeClassName)) {
             return String.class;
-        }else {
+        } else {
             return Object.class;
         }
     }
@@ -68,42 +68,45 @@ public class Utils {
     public static String getTypeName(TypeMirror fieldType) {
         TypeKind typeKind = fieldType.getKind();
         String typeClassName = fieldType.toString();
-        if(typeKind == TypeKind.BOOLEAN || Boolean.class.getName().equals(typeClassName)){
+        if (typeKind == TypeKind.BOOLEAN || Boolean.class.getName().equals(typeClassName)) {
             return "Boolean";
-        }else if(typeKind == TypeKind.INT || Integer.class.getName().equals(typeClassName)){
+        } else if (typeKind == TypeKind.INT || Integer.class.getName().equals(typeClassName)) {
             return "Int";
-        }else if(typeKind == TypeKind.LONG || Long.class.getName().equals(typeClassName)){
+        } else if (typeKind == TypeKind.LONG || Long.class.getName().equals(typeClassName)) {
             return "Long";
-        }else if(typeKind == TypeKind.FLOAT || Float.class.getName().equals(typeClassName)){
+        } else if (typeKind == TypeKind.FLOAT || Float.class.getName().equals(typeClassName)) {
             return "Float";
-        }else if(String.class.getName().equals(typeClassName)){
+        } else if (String.class.getName().equals(typeClassName)) {
             return "String";
-        }else {
+        } else {
             return "";
         }
     }
-    private static String getDefaultValue(TypeMirror fieldType){
+
+    private static String getDefaultValue(TypeMirror fieldType) {
         //TODO hard code here, will support later
         TypeKind typeKind = fieldType.getKind();
         String typeClassName = fieldType.toString();
-        if(typeKind == TypeKind.BOOLEAN || Boolean.class.getName().equals(typeClassName)){
+        if (typeKind == TypeKind.BOOLEAN || Boolean.class.getName().equals(typeClassName)) {
             return "false";
-        }else if(typeKind == TypeKind.INT || Integer.class.getName().equals(typeClassName)){
+        } else if (typeKind == TypeKind.INT || Integer.class.getName().equals(typeClassName)) {
             return "0";
-        }else if(typeKind == TypeKind.LONG || Long.class.getName().equals(typeClassName)){
+        } else if (typeKind == TypeKind.LONG || Long.class.getName().equals(typeClassName)) {
             return "0";
-        }else if(typeKind == TypeKind.FLOAT || Float.class.getName().equals(typeClassName)){
+        } else if (typeKind == TypeKind.FLOAT || Float.class.getName().equals(typeClassName)) {
             return "0";
-        }else if(String.class.getName().equals(typeClassName)){
+        } else if (String.class.getName().equals(typeClassName)) {
             return "\"\"";
-        }else {
+        } else {
             return "";
         }
     }
+
     public static String getSpSetterStatement(TypeMirror typeMirror, String fieldName, String parameter) {
         String format = "spHelper.set%s(\"%s\", %s)";
         return String.format(Locale.US, format, getTypeName(typeMirror), fieldName, parameter);
     }
+
     public static String getSpGetterStatement(TypeMirror typeMirror, String fieldName, String parameter) {
         String format = "return spHelper.get%s(\"%s\", %s)";
         return String.format(Locale.US, format, getTypeName(typeMirror), fieldName, getDefaultValue(typeMirror));
