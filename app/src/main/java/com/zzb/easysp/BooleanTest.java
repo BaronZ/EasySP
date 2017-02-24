eb524905a9eac83c7544b17e20389e20813bb068
zzb <realbo89@outlook.com>
2017-02-24 18:25:08 +0800

    test

---
+++ app/src/main/java/com/zzb/easysp/BooleanTest.java
@@ -0,0 +1,36 @@
+package com.zzb.easysp;
+
+/**
+ * Created by ZZB on 2017/1/24.
+ */
+@EasySP
+public class BooleanTest {
+
+    private boolean normal;
+    private boolean isBoolean;
+    private boolean isiah;
+
+    public boolean isiah() {
+        return isiah;
+    }
+
+    public void setIsiah(boolean isiah) {
+        this.isiah = isiah;
+    }
+
+    public boolean isBoolean() {
+        return isBoolean;
+    }
+
+    public void setBoolean(boolean aBoolean) {
+        isBoolean = aBoolean;
+    }
+
+    public boolean isNormal() {
+        return normal;
+    }
+
+    public void setNormal(boolean normal) {
+        this.normal = normal;
+    }
+}
